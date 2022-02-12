98238cc3bc636698a73e861bea6a84b525d3c9cd
Baris von Kayadelen <baris@kayadelen.com>
2022-02-12 18:29:05 +0200

    Initial

---
+++ billcal_electric-1.py
@@ -0,0 +1,35 @@
+# Up to 210kWh consumption 1kWh = 1.37 TL
+# After 210kWh consumption 1kWh = 2.055 TL
+tarif = 1.37
+limit = 210
+
+# Bill Function
+def ftr(a,b) :
+    if a <= limit :
+        c = a * b
+        return c
+    else :
+        c = (limit * b) + ((a - limit) * b * 1.5)
+        return c 
+
+inp_ilk = input('\nEnter first meter value: ')
+try :
+    ilk = float(inp_ilk)
+except :
+    print('\n!!! Please enter numerical value!\n')
+    quit()
+
+inp_son = input('Enter last meter value: ')
+try :
+    son = float(inp_son)
+except :
+    print('\n!!! Please enter numerical value!\n')
+    quit()
+
+tuketim = son - ilk
+fatura =  round(ftr(tuketim,tarif),2)
+
+if tuketim >= 0 :
+    print('\nConsumption amount:',tuketim,'kWh and your bill is', fatura,'TL. \n')
+else :
+    print('\nError!!! The last reading can not be smaller than the first reading. Please check your values!\n')
---
+++ billcal_electric-2.py
@@ -0,0 +1,23 @@
+# Up to 210kWh consumption 1kWh = 1.37 TL
+# After 210kWh consumption 1kWh = 2.055 TL
+tarif = 1.37
+limit = 210
+
+# Fatura fonksiyonu
+def tktm(a) :
+    if a < (tarif * limit) :
+        a = a / 1.37
+        return a
+    else :
+        b = limit + ((a - (tarif * limit)) / (tarif * 1.5))
+        return b
+
+inp_ftr = input('\nEnter the invoice amount: ')
+try :
+    ftr = float(inp_ftr)
+except :
+    print('\n!!! Please enter numerical value!\n')
+    quit()
+
+tuketim = round(tktm(ftr))
+print('\nConsumption amaount is ',tuketim,'kWh.\n')
