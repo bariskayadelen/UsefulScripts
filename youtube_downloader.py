359bb3bb37426534e1aa0d19bd8b403128104513
Baris Kayadelen <baris@kayadelen.com>
2022-08-05 12:25:24 +0300

    Youtube script added

---
+++ youtube_downloader.py
@@ -0,0 +1,11 @@
+import pytube
+link = input('Enter The Youtube Video URL : ')
+yt = pytube.YouTube(link)
+print("Title:", yt.title)
+print("Author:", yt.author)
+print("Published date:", yt.publish_date.strftime("%Y-%m-%d"))
+print("Number of views:", yt.views)
+print("Length of video:", yt.length, "seconds")
+# yt.streams.first().download()
+yt.streams.get_highest_resolution().download()
+print("Video successfullly downloaded from", link)
